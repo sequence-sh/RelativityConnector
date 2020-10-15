ffcc9e38ecfafdc6ea6b82479838b56b05549920
Mark Wainwright <wainwrightml@gmail.com>
2020-10-15 14:34:38 +0100

    Added document file manager

---
+++ Relativity/DocumentFileManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Flurl;
+using Flurl.Http;
+
+namespace Reductech.Connectors.Relativity
+{
+    public static class DocumentFileManager
+    {
+        public static async Task<Result<string, FlurlHttpException>> DownloadFile(IRelativitySettings settings, int workspaceId, int documentArtifactId, CancellationToken cancellationToken)
+        {
+            var urlSuffix = $"/Relativity.REST/api/Relativity.Document/workspace/{workspaceId}/downloadnativefile/{documentArtifactId}";
+
+            var url = Url.Combine(settings.Url, urlSuffix);
+
+
+            string resultString;
+
+            try
+            {
+                resultString = await url.WithHeader("X-CSRF-Header", "-")
+                .WithBasicAuth(settings.RelativityUsername, settings.RelativityPassword)
+                .GetStringAsync(cancellationToken);
+            }
+            catch (FlurlHttpException e)
+            {
+                return Result.Failure<string, FlurlHttpException>(e);
+            }
+
+            return resultString;
+        }
+
+
+    }
+}
