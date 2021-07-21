401e0e87647cc0b2090b7d83161fa9e261bf21f2
Mark Wainwright <wainwrightml@gmail.com>
2021-07-21 16:11:08 +0100

    Added DeleteWorkspace step

--- Relativity.Tests/Steps/RelativityCreateWorkspaceTests.cs
+++ Relativity.Tests/Steps/RelativityCreateWorkspaceTests.cs
@@ -10,62 +10,56 @@ namespace Reductech.EDR.Connectors.Relativity.Tests.Steps
 {
     public partial class RelativityCreateWorkspaceTests : StepTestBase<RelativityCreateWorkspace, Entity>
     {
-
         /// <inheritdoc />
         protected override IEnumerable<StepCase> StepCases
         {
             get
             {
-
-
                 var expectedEntityString =
                     "(Client: (Secured: False Value: (Name: \"Relativity\" ArtifactID: 1015644 Guids: \"\")) ClientNumber: (Secured: False Value: \"Relativity\") DownloadHandlerUrl: \"Relativity.Distributed\" EnableDataGrid: False Matter: (Secured: False Value: (Name: \"Salt vs. Pepper\" ArtifactID: 1016816 Guids: \"\")) MatterNumber: (Secured: False Value: \"1\") ProductionRestrictions: (Secured: False Value: (Name: \"Saved Search 1\" ArtifactID: 1117132 Guids: \"\")) ResourcePool: (Secured: False Value: (Name: \"Default\" ArtifactID: 1015040 Guids: \"\")) DefaultFileRepository: (Secured: False Value: (Name: \"\\\\\\\\A-BC-DE-FGHIJK\\\\fileshare\\\\\" ArtifactID: 1014887 Guids: \"\")) DataGridFileRepository: \"\" DefaultCacheLocation: (Secured: False Value: (Name: \"Default Cache Location\" ArtifactID: 1015534 Guids: \"\")) SqlServer: (Secured: False Value: (Name: \"A-BC-DE-FGHIJK\\\\EDDSINSTANCE001\" ArtifactID: 1015096 Guids: \"\")) AzureCredentials: \"\" AzureFileSystemCredentials: \"\" SqlFullTextLanguage: (Name: \"English\" ID: 1033) Status: (Name: \"Active\" ArtifactID: 675 Guids: \"\") WorkspaceAdminGroup: \"\" Keywords: \"Sample keywords for a workspace\" Notes: \"Sample notes for a workspace\" CreatedOn: 2018-04-24T15:19:57.6770000 CreatedBy: (Name: \"Admin, Relativity\" ArtifactID: 9 Guids: \"\") LastModifiedBy: (Name: \"Admin, Relativity\" ArtifactID: 9 Guids: \"\") LastModifiedOn: 2020-01-09T15:51:39.1300000 Meta: \"\" Actions: \"\" Name: \"Sample workspace\" ArtifactID: 1017266 Guids: \"\")";
-                
+
 
                 var flurlClientFactory = new TestFlurlClientFactory();
 
                 flurlClientFactory.HttpTest
                     .ForCallsTo("http://TestRelativityServer/Relativity.Rest/API/relativity-environment/v1/workspace")
-
                     .RespondWith(ResponseJson);
 
                 flurlClientFactory.HttpTest.RespondWith(status: 417);
                 //flurlClientFactory.HttpTest
 
                 yield return new StepCase(
-                            "Export with condition",
-                            new Log<Entity>()
+                        "Export with condition",
+                        new Log<Entity>()
+                        {
+                            Value = new RelativityCreateWorkspace()
                             {
-                                Value = new RelativityCreateWorkspace()
-                                {
-                                    Name = Constant("MyNewWorkspace"),
-                                    DefaultCacheLocationId = Constant(1),
-                                    DefaultFileRepositoryId = Constant(2),
-                                    MatterId = Constant(3),
-                                    ResourcePoolId = Constant(4),
-                                    SqlServerId = Constant(5),
-                                    StatusId = Constant(6),
-                                    TemplateId = Constant(7)
-
-                                }
-                            }, Unit.Default,
-                            expectedEntityString
-                        )
-                        .WithRelativitySettings<RelativityCreateWorkspace, Entity, StepCase>(
-                            new RelativitySettings()
-                            {
-                                RelativityUsername = "UN",
-                                RelativityPassword = "PW",
-                                Url = "http://TestRelativityServer"
+                                WorkspaceName = Constant("MyNewWorkspace"),
+                                DefaultCacheLocationId = Constant(1),
+                                DefaultFileRepositoryId = Constant(2),
+                                MatterId = Constant(3),
+                                ResourcePoolId = Constant(4),
+                                SqlServerId = Constant(5),
+                                StatusId = Constant(6),
+                                TemplateId = Constant(7)
                             }
-                        )
-                        .WithContext(
-                            ConnectorInjection.FlurlClientFactoryKey,
-                            flurlClientFactory
-                        );
+                        }, Unit.Default,
+                        expectedEntityString
+                    )
+                    .WithRelativitySettings<RelativityCreateWorkspace, Entity, StepCase>(
+                        new RelativitySettings()
+                        {
+                            RelativityUsername = "UN",
+                            RelativityPassword = "PW",
+                            Url = "http://TestRelativityServer"
+                        }
+                    )
+                    .WithContext(
+                        ConnectorInjection.FlurlClientFactoryKey,
+                        flurlClientFactory
+                    );
             }
         }
-
 
 
         public const string ResponseJson = @"{
---
+++ Relativity/Steps/RelativityDeleteWorkspace.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using CSharpFunctionalExtensions;
+using Flurl;
+using Flurl.Http;
+using Reductech.EDR.Connectors.Relativity.Errors;
+using Reductech.EDR.Core;
+using Reductech.EDR.Core.Attributes;
+using Reductech.EDR.Core.Internal;
+using Reductech.EDR.Core.Internal.Errors;
+using Reductech.EDR.Core.Util;
+
+namespace Reductech.EDR.Connectors.Relativity.Steps
+{
+    /// <summary>
+    /// Deletes a relativity workspace
+    /// </summary>
+    [SCLExample("RelativityDeleteWorkspace 42",ExecuteInTests = false, Description = "Deletes workspace 42")]
+    public sealed class RelativityDeleteWorkspace : CompoundStep<Unit>
+    {
+        protected override async Task<Result<Unit, IError>> Run(IStateMonad stateMonad,
+            CancellationToken cancellationToken)
+        {
+            var workspaceId = await WorkspaceId.Run(stateMonad, cancellationToken);
+            if (workspaceId.IsFailure) return workspaceId.ConvertFailure<Unit>();
+
+            var settingsResult = stateMonad.Settings.TryGetRelativitySettings();
+            if (settingsResult.IsFailure)
+                return settingsResult.MapError(x => x.WithLocation(this)).ConvertFailure<Unit>();
+
+            var flurlClientResult = stateMonad.GetFlurlClientFactory().Map(x => x.FlurlClient);
+            if (flurlClientResult.IsFailure)
+                return flurlClientResult.MapError(x => x.WithLocation(this)).ConvertFailure<Unit>();
+
+
+            var url = Url.Combine(settingsResult.Value.Url,
+                "Relativity.Rest",
+                "API",
+                "relativity-environment",
+                $"v{settingsResult.Value.APIVersionNumber}",
+                "workspace",
+                workspaceId.Value.ToString()
+            );
+
+            IFlurlResponse response;
+
+            try
+            {
+                response =
+                    await url
+                        .WithBasicAuth(settingsResult.Value.RelativityUsername, settingsResult.Value.RelativityPassword)
+                        .WithHeader("X-CSRF-Header", "-")
+                        .WithClient(flurlClientResult.Value)
+                        .DeleteAsync(cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                var error = ErrorCode.Unknown.ToErrorBuilder(ex).WithLocation(this);
+                return Result.Failure<Unit, IError>(error);
+            }
+
+            if (!response.ResponseMessage.IsSuccessStatusCode)
+                return Result.Failure<Unit, IError>(
+                    ErrorCode_Relativity.RequestFailed
+                        .ToErrorBuilder(response.StatusCode, response.ResponseMessage.ReasonPhrase)
+                        .WithLocation(this)
+                );
+
+            return Unit.Default;
+        }
+
+
+        /// <summary>
+        /// The id of the workspace to delete
+        /// </summary>
+        [StepProperty(1)]
+        [Required]
+        public IStep<int> WorkspaceId { get; set; } = null!;
+
+        public override IStepFactory StepFactory { get; } = new SimpleStepFactory<RelativityDeleteWorkspace, Unit>();
+    }
+}
