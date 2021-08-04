98c68f0817acf8911440b21e0ef8b312ced8c7c8
Mark Wainwright <wainwrightml@gmail.com>
2021-08-04 17:12:46 +0100

    Tidied up integration tests

--- Relativity.Tests/ExampleTests.cs
+++ Relativity.Tests/ExampleTests.cs
@@ -20,148 +20,151 @@ using static Reductech.EDR.Core.TestHarness.StaticHelpers;
 
 namespace Reductech.EDR.Connectors.Relativity.Tests
 {
-    [AutoTheory.UseTestOutputHelper]
-    public partial class ExampleTests
+
+[AutoTheory.UseTestOutputHelper]
+public partial class ExampleTests
+{
+    public static IEnumerable<IStep> IntegrationTestCases
     {
-        public const string CreateRDOsSCL = @"
-RelativityCreateDynamicObjects
-    WorkspaceArtifactId: 1018846
-    Entities: [(Name: 'My Entity')]
-    ArtifactTypeId: 10
-
-";
-
-        public static IEnumerable<IStep> IntegrationTestCases
+        get
         {
-            get
+            //yield return new RelativityQueryDocuments()
+            //{
+            //    ArtifactTypeId = Constant(10),
+            //    Condition = Constant("'Title' LIKE 'Bond'"),
+            //    WorkspaceArtifactId = Constant(1019032),
+            //    Start = Constant(0),
+            //    Length = Constant(100)
+            //};
+
+            //yield break;
+
+            yield return new Sequence<Unit>()
             {
-                //yield return new RelativityDeleteMatter()
-                //{
-                //    MatterArtifactId = Constant(1018999)
-                //};
-
-                //yield return new RelativityDeleteWorkspace()
-                //{
-                //    WorkspaceId = Constant(1018994)
-                //};
-                //yield break;
-                yield return new Sequence<Unit>()
+                InitialSteps = new List<IStep<Unit>>()
                 {
-                    InitialSteps = new List<IStep<Unit>>()
+                    new SetVariable<int>()
                     {
-                        new SetVariable<int>()
+                        Value = new RelativityCreateMatter()
                         {
-                            Value = new RelativityCreateMatter()
-                            {
-                                ClientId = Constant(1018410),
-                                StatusId = Constant(671),
-                                MatterName = Constant("Test Matter"),
-                                Number = Constant("Ten"),
-                                Keywords = Constant("Test Keywords"),
-                                Notes = Constant("Test Notes")
-                            },
-                            Variable = new VariableName("MatterId")
+                            ClientId   = Constant(1018410),
+                            StatusId   = Constant(671),
+                            MatterName = Constant("Test Matter"),
+                            Number     = Constant("Ten"),
+                            Keywords   = Constant("Test Keywords"),
+                            Notes      = Constant("Test Notes")
                         },
-                        new SetVariable<Entity>()
+                        Variable = new VariableName("MatterId")
+                    },
+                    new SetVariable<Entity>()
+                    {
+                        Variable = new VariableName("Workspace"),
+                        Value = new RelativityCreateWorkspace()
                         {
-                            Variable = new VariableName("Workspace"),
-                            Value = new RelativityCreateWorkspace()
-                            {
-                                WorkspaceName = Constant("Integration Test Workspace"),
-                                MatterId = GetVariable<int>("MatterId"),
-                                TemplateId = Constant(1015024),
-                                StatusId = Constant(675),
-                                ResourcePoolId = Constant(1015040),
-                                SqlServerId = Constant(1015096),
-                                DefaultFileRepositoryId = Constant(1014887),
-                                DefaultCacheLocationId = Constant(1015534)
-                            }
-                        },
-                        new RelativityImport()
-                        {
-                            FilePath = Constant(@"C:\Users\wainw\source\repos\Examples\Concordance\Carla2\loadfile.dat"),
-                            FileImportType = Constant(FileImportType.Object),
-                            SettingsFilePath = Constant(@"C:\Users\wainw\source\repos\Examples\Concordance\CarlaSettings.kwe"),
-                            StartLineNumber = null,
-                            WorkspaceId = new EntityGetValue<int>()
-                            {
-                                Property = Constant("ArtifactID"), 
-                                Entity = new GetVariable<Entity>()
-                                {
-                                    Variable = new VariableName("Workspace")
-                                }
-                            }
-                        },
-                        new RelativityDeleteWorkspace()
-                        {
-                            WorkspaceId = new EntityGetValue<int>()
-                            {
-                                Property = Constant("ArtifactID"), 
-                                Entity = new GetVariable<Entity>()
-                                {
-                                    Variable = new VariableName("Workspace")
-                                }
-                            }
-                        },
-                        new RelativityDeleteMatter()
-                        {
-                            MatterArtifactId = GetVariable<int>("MatterId"),
+                            WorkspaceName           = Constant("Integration Test Workspace"),
+                            MatterId                = GetVariable<int>("MatterId"),
+                            TemplateId              = Constant(1015024),
+                            StatusId                = Constant(675),
+                            ResourcePoolId          = Constant(1015040),
+                            SqlServerId             = Constant(1015096),
+                            DefaultFileRepositoryId = Constant(1014887),
+                            DefaultCacheLocationId  = Constant(1015534)
                         }
-
-                    }
-                };
-            }
+                    },
+                    new RelativityImport()
+                    {
+                        FilePath =
+                            Constant(
+                                @"C:\Users\wainw\source\repos\Examples\Concordance\Carla2\loadfile.dat"
+                            ),
+                        FileImportType = Constant(FileImportType.Object),
+                        SettingsFilePath =
+                            Constant(
+                                @"C:\Users\wainw\source\repos\Examples\Concordance\CarlaSettings.kwe"
+                            ),
+                        StartLineNumber = null,
+                        WorkspaceId = new EntityGetValue<int>()
+                        {
+                            Property = Constant("ArtifactID"),
+                            Entity = new GetVariable<Entity>()
+                            {
+                                Variable = new VariableName("Workspace")
+                            }
+                        }
+                    },
+                    //new RelativityDeleteWorkspace()
+                    //{
+                    //    WorkspaceId =  new EntityGetValue<int>()
+                    //    {
+                    //        Property = Constant("ArtifactID"),
+                    //        Entity = new GetVariable<Entity>()
+                    //        {
+                    //            Variable = new VariableName("Workspace")
+                    //        }
+                    //    }
+                    //},
+                    //new RelativityDeleteMatter()
+                    //{
+                    //    MatterArtifactId = GetVariable<int>("MatterId"),
+                    //}
+                }
+            };
         }
+    }
 
-        public static IEnumerable<object[]> IntegrationTestCaseArgs =>
-            IntegrationTestCases.Select(x => new[] { x.Serialize() });
+    public static IEnumerable<object[]> IntegrationTestCaseArgs =>
+        IntegrationTestCases.Select(x => new[] { x.Serialize() });
 
+    //[Theory(Skip = "Manual")]
+    [Theory]
+    [Trait("Category", "Integration")]
+    [MemberData(nameof(IntegrationTestCaseArgs))]
+    public async Task RunSCLSequence(string scl)
+    {
+        var logger =
+            TestOutputHelper.BuildLogger(new LoggingConfig() { LogLevel = LogLevel.Trace });
 
-        //[Theory(Skip = "Manual")]
-        [Theory]
-        [Trait("Category", "Integration")]
-        [MemberData(nameof(IntegrationTestCaseArgs))]
-        public async Task RunSCLSequence(string scl)
-        {
-            var logger =
-                TestOutputHelper.BuildLogger(new LoggingConfig() { LogLevel = LogLevel.Information });
+        logger.LogInformation(scl);
 
-            logger.LogInformation(scl);
-
-            var sfs = StepFactoryStore.Create(new ConnectorData(
+        var sfs = StepFactoryStore.Create(
+            new ConnectorData(
                 new ConnectorSettings()
                 {
-                    Id = "Reductech.EDR.Connectors.Relativity",
-                    Enable = true,
+                    Id      = "Reductech.EDR.Connectors.Relativity",
+                    Enable  = true,
                     Version = "0.10.0",
                     Settings = new RelativitySettings()
                     {
                         RelativityUsername = "relativity.admin@relativity.com",
                         RelativityPassword = "Test1234!",
-                        Url = "http://relativitydevvm/",
-                        APIVersionNumber = 1,
-                        DesktopClientPath = @"C:\Program Files\kCura Corporation\Relativity Desktop Client\Relativity.Desktop.Client.exe"
+                        Url                = "http://relativitydevvm/",
+                        APIVersionNumber   = 1,
+                        DesktopClientPath =
+                            @"C:\Program Files\kCura Corporation\Relativity Desktop Client\Relativity.Desktop.Client.exe"
                     }.ToDictionary()
                 },
                 typeof(RelativityGetClients).Assembly
-            ));
+            )
+        );
 
-            var runner = new SCLRunner(
-                logger,
-                sfs,
-                ExternalContext.Default with
-                {
-                    InjectedContexts = new ConnectorInjection().TryGetInjectedContexts().Value.ToArray()
-                }
-            );
+        var runner = new SCLRunner(
+            logger,
+            sfs,
+            ExternalContext.Default with
+            {
+                InjectedContexts = new ConnectorInjection().TryGetInjectedContexts()
+                    .Value.ToArray()
+            }
+        );
 
-            var r = await runner.RunSequenceFromTextAsync(
-                scl,
-                new Dictionary<string, object>(),
-                CancellationToken.None
-            );
+        var r = await runner.RunSequenceFromTextAsync(
+            scl,
+            new Dictionary<string, object>(),
+            CancellationToken.None
+        );
 
-            r.ShouldBeSuccessful();
-        }
+        r.ShouldBeSuccessful();
     }
+}
+
 }
--- Relativity/Steps/RelativityAPIRequest.cs
+++ Relativity/Steps/RelativityAPIRequest.cs
@@ -13,9 +13,11 @@ using Entity = Reductech.EDR.Core.Entity;
 
 namespace Reductech.EDR.Connectors.Relativity.Steps
 {
+
     public abstract class RelativityApiRequest<TRequest, TService, TServiceOutput, TOutput> : CompoundStep<TOutput>
-        where TService : IDisposable
+    where TService : IDisposable
     {
+
         protected static Result<Array<Entity>, IErrorBuilder> TryConvertToEntityArray<T>(IEnumerable<T> stuff)
         {
             var array = new List<Entity>();
@@ -26,60 +28,104 @@ namespace Reductech.EDR.Connectors.Relativity.Steps
                 array.Add(r.Value);
             }
 
-            return array.ToSCLArray();
-        }
+        return array.ToSCLArray();
+    }
 
-        protected static Result<Entity, IErrorBuilder> TryConvertToEntity<T>(T thing)
+        protected static  Result<Entity, IErrorBuilder> TryConvertToEntity<T>(T thing)
         {
             var json = JsonConvert.SerializeObject(thing);
 
-            var responseEntity = JsonConvert.DeserializeObject<Entity>(json,
-                EntityJsonConverter.Instance, new VersionConverter());
+        var responseEntity = JsonConvert.DeserializeObject<Entity>(
+            json,
+            EntityJsonConverter.Instance,
+            new VersionConverter()
+        );
 
-            if (responseEntity is null)
-                return ErrorCode.CouldNotParse.ToErrorBuilder(json, nameof(Entity));
+        if (responseEntity is null)
+            return ErrorCode.CouldNotParse.ToErrorBuilder(json, nameof(Entity));
 
             return responseEntity;
+
         }
 
+        /// <summary>
+        /// Get a service from the Relativity proxy
+        /// </summary>
+        protected Result<TService2, IErrorBuilder> TryGetService<TService2>(IStateMonad stateMonad)
+        where TService2: IDisposable
+        {
+            var settingsResult = stateMonad.Settings.TryGetRelativitySettings();
+            if (settingsResult.IsFailure)
+                return settingsResult.ConvertFailure<TService2>();
+
+            var serviceFactoryFactory = stateMonad.ExternalContext.TryGetContext<IServiceFactoryFactory>(ConnectorInjection.ServiceFactoryFactoryKey);
+
+            if (serviceFactoryFactory.IsFailure) 
+                return serviceFactoryFactory.ConvertFailure<TService2>();
+
+            var serviceFactory = serviceFactoryFactory.Value.CreateServiceFactory(settingsResult.Value);
+
+            TService2 service;
+
+            try
+            {
+                service = serviceFactory.CreateProxy<TService2>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+
+            return service;
+        }
 
         /// <inheritdoc />
-        protected override async Task<Result<TOutput, IError>> Run(IStateMonad stateMonad,
-            CancellationToken cancellationToken)
+        protected override async Task<Result<TOutput, IError>> Run(IStateMonad stateMonad, CancellationToken cancellationToken)
         {
             var requestObjectResult = await TryCreateRequest(stateMonad, cancellationToken);
             if (requestObjectResult.IsFailure) return requestObjectResult.ConvertFailure<TOutput>();
 
+        if (requestObjectResult.IsFailure)
+            return requestObjectResult.ConvertFailure<TOutput>();
 
-            var serviceResult = stateMonad.TryGetService<TService>();
-            if (serviceResult.IsFailure)
-                return serviceResult.MapError(x => x.WithLocation(this)).ConvertFailure<TOutput>();
+            var serviceResult = TryGetService<TService>(stateMonad);
+            if (serviceResult.IsFailure) return serviceResult.MapError(x=>x.WithLocation(this)) .ConvertFailure<TOutput>();
 
-            using var service = serviceResult.Value;
+        if (serviceResult.IsFailure)
+            return serviceResult.MapError(x => x.WithLocation(this)).ConvertFailure<TOutput>();
 
-            TServiceOutput serviceOutput;
+        using var service = serviceResult.Value;
+
+        TServiceOutput serviceOutput;
 
             try
             {
-                serviceOutput = await SendRequest(stateMonad, serviceResult.Value, requestObjectResult.Value,
-                    cancellationToken);
+                serviceOutput = await SendRequest(stateMonad, serviceResult.Value, requestObjectResult.Value, cancellationToken);
+
             }
             catch (Exception ex)
             {
                 return Result.Failure<TOutput, IError>(ErrorCode.Unknown.ToErrorBuilder(ex).WithLocation(this));
             }
 
-            var output = ConvertOutput(serviceOutput).MapError(x => x.WithLocation(this));
+            var output = ConvertOutput(serviceOutput).MapError(x=>x.WithLocation(this));
 
-            return output;
-        }
-
-        public abstract Result<TOutput, IErrorBuilder> ConvertOutput(TServiceOutput serviceOutput);
-
-        public abstract Task<TServiceOutput> SendRequest(IStateMonad stateMonad, TService service,
-            TRequest requestObject, CancellationToken cancellationToken);
-
-        public abstract Task<Result<TRequest, IError>> TryCreateRequest(IStateMonad stateMonad,
-            CancellationToken cancellation);
+        return output;
     }
+
+    public abstract Result<TOutput, IErrorBuilder> ConvertOutput(TServiceOutput serviceOutput);
+
+    public abstract Task<TServiceOutput> SendRequest(
+        IStateMonad stateMonad,
+        TService service,
+        TRequest requestObject,
+        CancellationToken cancellationToken);
+
+    public abstract Task<Result<TRequest, IError>> TryCreateRequest(
+        IStateMonad stateMonad,
+        CancellationToken cancellation);
+}
+
 }
