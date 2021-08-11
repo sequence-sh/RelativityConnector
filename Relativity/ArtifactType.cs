251a18a98289e55efb227286381fae96e58061ac
Mark Wainwright <wainwrightml@gmail.com>
2021-08-11 10:32:44 +0100

    Changes to ArtifactType

--- Relativity/ArtifactType.cs
+++ Relativity/ArtifactType.cs
@@ -1,34 +1,43 @@
 ﻿namespace Reductech.EDR.Connectors.Relativity
 {
 
+/// <summary>
+/// Relativity Artifact Type
+/// </summary>
 public enum ArtifactType
 {
     //https://platform.relativity.com/RelativityOne/index.htm#../Subsystems/rsapiclasses/Content/html/T_kCura_Relativity_Client_ArtifactType.htm
 
-    Batch = 27,
-    BatchSet = 24,
-    Case = 8,
-    Client = 5,
-    Code = 7,
-    Document = 10,
-    Error = 18,
-    Field = 14,
-    Folder = 9,
+
+    User = 2,
     Group = 3,
-    Layout = 16,
+    View = 4,
+    Client = 5,
     Matter = 6,
-    MarkupSet = 22,
+    Code = 7,
+    /// <summary>
+    /// Case or Workspace
+    /// </summary>
+    Case = 8,
+    Folder = 9,
+    Document = 10,
+    Document = 10,
+    
+    Field = 14,
+    Search = 15,
+    Layout = 16,
     Production = 17,
+    Error = 18,
+    MarkupSet = 22,
+    Tab = 23,
+    BatchSet = 24,
     ObjectType = 25,
+    SearchContainer = 26,
+    Batch = 27,
     RelativityScript = 28,
+    SearchIndex = 29,
     ResourcePool = 31,
     ResourceServer = 32,
-    SearchIndex = 29,
-    Search = 15,
-    Tab = 23,
-    User = 2,
-    View = 4,
-    SearchContainer = 26,
     InstanceSetting = 42,
     Credential = 43,
 }
